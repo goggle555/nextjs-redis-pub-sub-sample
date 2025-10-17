69ac3fe2da36a277b04685823911018552f787e1
goggle555 <goggle555@fives.tech>
2025-10-17 17:25:48 +0900

    feat: top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="m-2">
+      <p className="font-bold text-2xl">Next.js Redis Pub/Sub Sample</p>
+      <ul className="list-disc list-inside">
+        <li>
+          <a className="underline text-blue-500" href="/publish">
+            Publish Page
+          </a>
+        </li>
+        <li>
+          <a className="underline text-blue-500" href="/subscribe">
+            Subscribe Page
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
